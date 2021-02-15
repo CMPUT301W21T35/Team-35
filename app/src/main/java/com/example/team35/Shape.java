d0bfc2df801ea382d5992790b823aedfc5f5cb04
hbi-uab <hbi@ualberta.ca>
2021-02-15 14:59:31 -0700

    Update Shape.java

--- app/src/main/java/com/example/team35/Shape.java
+++ app/src/main/java/com/example/team35/Shape.java
@@ -2,7 +2,7 @@ package com.example.team35;
 
 public abstract class Shape {
 
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    private String color = "blue";
     
     public Shape(int x, int y) {
         
